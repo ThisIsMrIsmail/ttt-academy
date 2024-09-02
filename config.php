238137d16b5a5bdc554ca0b534ee53ba8d42980c
Ismail Sherif <ismailsherifwork@gmail.com>
2024-09-02 17:51:32 +0300

    Update SMTP email and name in config.php

--- config.php
+++ config.php
@@ -7,8 +7,8 @@ $_ENV["PASSWORD"] = "";
 $_ENV["DATABASE_NAME"] = "triblet";
 $_ENV["PORT"] = 3306;
 
-$_ENV["TTT_SMTP_NAME"] = "TribleTAcademy";
-$_ENV["TTT_SMTP_EMAIL"] = "TribleTAcademy@hotmail.com";
-$_ENV["TTT_SMTP_EMAIL_PASSWORD"] = "password#ttt";
+$_ENV["TTT_SMTP_NAME"] = "Triple T Academy";
+$_ENV["TTT_SMTP_EMAIL"] = "tribletacademy@hotmail.com";
+$_ENV["TTT_SMTP_EMAIL_PASSWORD"] = "";
 
 ?>
